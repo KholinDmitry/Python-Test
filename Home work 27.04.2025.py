7bde23d10721eb2f9c392920bf6582979dd7fcbb
Dmitry <kholindmitry@gmail.com>
2025-05-09 17:45:12 +0300

    Home work 27.04.2025

---
+++ Home work 27.04.2025.py
@@ -0,0 +1,8 @@
+a = input("Как вас зовут? ")
+b = input("Сколько вам лет? ")
+c = input("Любите ли вы программировать? ")
+user_info = {"name": a, "age": b, "live programming": c}
+print(user_info.values())
+print(user_info)
+
+
